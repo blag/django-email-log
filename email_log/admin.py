6d10e4d0e40723f6b7e5bfc9c979cd7b2f52a5cc
blag <blag@users.noreply.github.com>
2025-07-18 17:15:15 -0700

    Add Log model to track ESP status and integrate with Anymail

--- email_log/admin.py
+++ email_log/admin.py
@@ -1,6 +1,69 @@
 from django.contrib import admin
 from django.template.defaultfilters import linebreaksbr
-from .models import Attachment, Email
+from django.utils.translation import gettext as _
+
+from .models import Attachment, Email, Log
+
+
+class LogInline(admin.StackedInline):
+    model = Log
+    readonly_fields = [
+        "type",
+        "timestamp",
+        "esp",
+        "event_id",
+        "reject_reason",
+        "mta_response",
+        "tags",
+        "user_agent",
+        "click_url",
+        "raw",
+    ]
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "type",
+                    "timestamp",
+                    "tags",
+                ),
+            },
+        ),
+        (
+            _("Details"),
+            {
+                "fields": (
+                    "esp",
+                    "event_id",
+                    "reject_reason",
+                    "mta_response",
+                ),
+                "classes": ["collapse"],
+            },
+        ),
+        (
+            _("Other"),
+            {
+                "fields": (
+                    "user_agent",
+                    "click_url",
+                    "raw",
+                ),
+                "classes": ["collapse"],
+            },
+        ),
+    )
+    extra = 0
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class AttachmentInline(admin.StackedInline):
@@ -27,6 +90,15 @@ class AttachmentInline(admin.StackedInline):
     )
     extra = 0
 
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 class EmailAdmin(admin.ModelAdmin):
     list_display = ["recipients", "from_email", "subject", "date_sent", "ok"]
@@ -44,6 +116,7 @@ class EmailAdmin(admin.ModelAdmin):
     ]
     inlines = [
         AttachmentInline,
+        LogInline,
     ]
     search_fields = ["subject", "body", "recipients", "cc_recipients", "bcc_recipients"]
     exclude = ["body"]
